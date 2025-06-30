4286f180c6289f3539b2da1facd9680e18c6cd86
ERBODY <150860506+ERBODY@users.noreply.github.com>
2025-07-01 02:38:55 +0300

    Fix base path for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/Fati-s-Birthday/',
+  base: '/',
   plugins: [
     react(),
     // The code below enables dev tools like taking screenshots of your site
